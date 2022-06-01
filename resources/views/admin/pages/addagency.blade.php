39a131bec05d3247c1d8e49091f876abce6667d7
ICOTSOLUTIONS <icotsolutions>
2022-06-02 00:32:35 +0500

    working

--- resources/views/admin/pages/addagency.blade.php
+++ resources/views/admin/pages/addagency.blade.php
@@ -10,6 +10,16 @@
         padding: 0 15px;
         height: 28px;
     }
+
+    .w-110 {
+        width: 110%;
+
+    }
+
+    .w-178 {
+        width: 178%;
+
+    }
 </style>
 <section>
     <div class="container">
@@ -22,20 +32,25 @@
                         </div>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-8">
+                                <h6 class="text-success">Enter Your Agency Details</h6>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Company Name
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Owner Name
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Company Type
-                                            <select name="" id="" class="form-control">
+                                            <select name="" id="" class="form-control w-110">
                                                 <option value="" selected>Select Company</option>
                                                 <option value="">RealState</option>
                                                 <option value="">Developer</option>
@@ -48,82 +63,74 @@
                                             </select>
                                         </label>
                                     </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Licence no
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Permit no
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Permit no
+                                            <input type="text" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Rera no
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Establishment Date:
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Licence Expire Date:
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
-                                    <div class="form-group">
-                                        <label>Access of Agents
-                                            <select name="" id="" class="form-control">
-                                                <option value="" selected>Select Agents</option>
-                                                <option value="">Three(3)</option>
-                                                <option value="">Six (6)</option>
-                                                <option value="">Ten (10)</option>
-                                                <option value="">Custom</option>
-                                            </select>
-                                        </label>
-                                    </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Country
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+
                                 </div>
-                                <div class="col-md-4">
-                                    <img src="{{asset('assets/img/users/user-4.png')}}" class="float-right" alt="logo" width=40%>
-                                    <div class="form-group">
-                                        <label>Email
-                                            <input type="email" class="form-control">
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Phone
-                                            <input type="text" class="form-control">
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Mobile
-                                            <input type="text" class="form-control">
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Website:(Optional)
-                                            <input type="text" class="form-control">
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Autorised
-                                            <input type="checkbox" class="form-control">
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4">
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>City
-                                            <select name="" id="" class="form-control">
+                                            <input type="text" class="form-control">
+                                        </label>
+                                    </div>
+
+                                </div>
+
+                                <!-- <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>City
+                                            <select name="" id="" class="form-control ">
                                                 <option value="" selected>Select City</option>
                                                 <option value="">lorem ipsum</option>
                                                 <option value="">lorem ipsum</option>
@@ -136,51 +143,123 @@
                                             </select>
                                         </label>
                                     </div>
+                                </div> -->
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Email
+                                            <input type="email" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Phone
+                                            <input type="text" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Mobile
+                                            <input type="text" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Website:(Optional)
+                                            <input type="text" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Street
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row">
+
+                                <div class="col-md-3">
                                     <div class="form-group">
                                         <label>Office
                                             <input type="text" class="form-control">
                                         </label>
                                     </div>
                                 </div>
-                                <div class="col-md-8">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Owner EID
+                                            <input type="file" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
                                     <div class="float-right">
                                         <div class="form-group">
                                             <label>Licence
                                                 <input type="file" class="form-control">
                                             </label>
                                         </div>
-                                        <div class="form-group">
-                                            <label>Owner Visa
-                                                <input type="file" class="form-control">
-                                            </label>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Owner EID
-                                                <input type="file" class="form-control">
-                                            </label>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>RERA
-                                                <input type="file" class="form-control">
-                                            </label>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Additional Documents
-                                                <input type="file" class="form-control">
-                                            </label>
-                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-md-4"></div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Owner Visa
+                                            <input type="file" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>RERA
+                                            <input type="file" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Additional Documents
+                                            <input type="file" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Autorised
+                                            <input type="checkbox" class="form-control">
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Access of Agents
+                                            <select name="" id="" class="form-control w-178">
+                                                <option value="" selected>Select Agents</option>
+                                                <option value="">Three(3)</option>
+                                                <option value="">Six (6)</option>
+                                                <option value="">Ten (10)</option>
+                                                <option value="">Custom</option>
+                                            </select>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-md-4">
+                                </div>
+                                <div class="col-md-2">
                                     <a href=""><button class="btn btn-success w-100">Save</button></a>
                                 </div>
-                                <div class="col-md-4"></div>
+                                <div class="col-md-14">
+                                </div>
                             </div>
                         </div>
                     </form>
