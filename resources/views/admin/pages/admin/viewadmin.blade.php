bd48eb05cf4a4319019c0618c20e78ad7117d68d
ICOTSOLUTIONS <icotsolutions@gmail.com>
2022-06-21 11:28:27 -0700

    workdone

--- resources/views/admin/pages/admin/viewadmin.blade.php
+++ resources/views/admin/pages/admin/viewadmin.blade.php
@@ -2,53 +2,65 @@
 @section('content')
 <style>
     .width {
-        width: 40%;
-        border-radius: 3rem;
+        width: 35%;
+        border-radius: 2rem;
     }
 </style>
-<div class="container bootstrap snippets bootdeys">
+<div class="container-fluid bootstrap snippets bootdeys">
     <div class="row">
         <div class="col-md-12">
-            <div class="card design">
-                <div class="card-header justify-content-center">
-                    <h4>
-                        VIEW ADMIN
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <h5>Personal Details</h5>
-                        <div class="col-md-3 text-align-right">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card design">
+                        <div class="card-header justify-content-center">
+                            <h4>Profile Details</h4>
+                        </div>
+                        <div class="text-center p-3">
                             <img src="{{asset('assets/img/bgimg.jpeg')}}" alt="img" class="width">
                         </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
+                        <div class="card-footer p-3 ">
+                            <div class="row mt-4">
+                                <h5>Personal Details</h5>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
 
-                        <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row mt-4">
-                        <h5>Account Details</h5>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                    </div>
-                    <div class="row mt-4">
-                        <h5>lorem Details</h5>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                    </div>
-                    <div class="row mt-4">
-                        <h5>lorem Details</h5>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
-                        <div class="col-md-3"><label for="">lorem</label></div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card design">
+                        <div class="card-header justify-content-center">
+                            <h4>
+                                VIEW ADMIN
+                            </h4>
+                        </div>
+                        <div class="card-body">
+
+                            <div class="row mt-4">
+                                <h5>Account Details</h5>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                            </div>
+                            <div class="row mt-4">
+                                <h5>lorem Details</h5>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                            </div>
+                            <div class="row mt-4">
+                                <h5>lorem Details</h5>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                                <div class="col-md-3"><label for="">lorem</label></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
