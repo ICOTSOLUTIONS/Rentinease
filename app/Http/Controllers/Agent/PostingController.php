56d7dd360e4f560fa8875c9ac6b2e27120298766
umer <umerwell1234@gmail.com>
2022-08-20 21:25:58 +0500

    working

--- app/Http/Controllers/Agent/PostingController.php
+++ app/Http/Controllers/Agent/PostingController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Agent;
 use App\Http\Controllers\Controller;
 use App\Models\Area;
 use App\Models\City;
+use App\Models\CoinDeduction;
 use App\Models\PlaceType;
+use App\Models\Posting;
 use App\Models\PropertyTypePlace;
 use App\Models\Purpose;
+use App\Models\User;
+use App\Models\UserCoins;
+use App\Models\UserPackageCoins;
 use Illuminate\Http\Request;
 
 class PostingController extends Controller
@@ -44,7 +49,42 @@ class PostingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+
+        ]);
+        $remain_coins = UserPackageCoins::where('user_id',auth()->user()->id)->first();
+        if($remain_coins->remain_coins >= 0){
+            $posting = new Posting();
+            $posting->purpose_id = $request->purpose_id;
+            $posting->property_type_place_id = $request->property_type_place_id;
+            $posting->area_id = $request->area_id;
+            $posting->layout = $request->layout;
+            $posting->bath = $request->bath;
+            $posting->size = $request->size;
+            $posting->building_name = $request->building_name;
+            $posting->building_age = $request->building_age;
+            $posting->price = $request->price;
+            $posting->price_per = $request->price_per;
+            if(isset($package_coins)){
+                $coins_deduct = CoinDeduction::where('id',$package_coins)->first();
+                if($coins_deduct->coins_deduct > $remain_coins->remain_coins){
+                    session()->flash('message', 'Please Buy Package First. Your Coins 0!');
+                    session()->flash('messageType', 'danger');
+                    return redirect()->back();
+                }
+                $remain_coins->remain_Coins = $remain_coins->remain_Coins - $coins_deduct->coins_deduct;
+                $remain_coins->save();
+                $user_coins = new UserCoins();
+                $user_coins->user_package_coins_id =  $remain_coins->id;
+                $user_coins->minus_coins =  $coins_deduct->coins_deduct;
+                $user_coins->save();
+            }
+            $posting->save();
+        }else{
+            session()->flash('message', 'Please Buy Package First. Your Coins 0!');
+            session()->flash('messageType', 'danger');
+            return redirect()->back();
+        }
     }
 
     /**
