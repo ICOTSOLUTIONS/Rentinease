3f5ac2df7032ed4e5add243e2f77ebd8338f344d
ICOTSOLUTIONS <icotsolutions@gmail.com>
2022-10-14 22:26:22 +0500

    working

--- resources/views/agency/layouts/master.blade.php
+++ resources/views/agency/layouts/master.blade.php
@@ -4,12 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Gati - Admin Dashboard Template</title>
+    <title>Agent/Agency Admin-Panel</title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assetagency/css/app.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assetagency/bundles/datatables/datatables.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('assetagency/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assetagency/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('assetagency/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assetagency/css/components.css') }}">
@@ -18,11 +17,13 @@
     <!-- <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' /> -->
     <!-- Map box style CSS -->
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
-    <link rel='stylesheet'
-        href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.css'
-        type='text/css' />
+    <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.css' type='text/css' />
 
 
+    <script src="http://cdn.pubnub.com/pubnub-3.7.1.min.js"></script>
+    <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js'></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.css' rel='stylesheet' />
 </head>
 <style>
     ::-webkit-scrollbar {
